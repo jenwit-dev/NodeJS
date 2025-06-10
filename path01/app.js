b5c5400b19ff2acdf7f5320d831c0872cbed3979
jenwit-dev <johnny.js19@gmail.com>
2025-06-11 03:31:18 +0700

    path module and unix and linux history

--- path01/app.js
+++ path01/app.js
@@ -1,9 +1,37 @@
 const path = require("path");
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 
 let thePath = __dirname + "/go/to/test.txt";
-console.log(thePath);
-console.log(path.dirname(thePath));
-console.log(path.basename(thePath));
+// console.log(thePath);
+// console.log(path.dirname(thePath));
+// console.log(path.basename(thePath));
+// console.log(path.extname(thePath));
+// console.log(path.join(__dirname, "data", "user"));
+// console.log(path.resolve());
+// console.log(path.resolve("data", "user"));
+// console.log(__dirname);
+// console.log(path.join("x", "y", "z"));
+// console.log(path.resolve());
+// console.log(__dirname);
+// console.log(process.cwd());
+// console.log("xxx");
+
+// project
+//  - src
+//  - dist // path01cd
+//      -build.js
+
+// console.log(path.resolve("src", "components", "../utils", "helper.js"));
+// Output (Unix-like): /Users/MyUser/Projects/my-app/src/utils/helper.js
+// (It processes from right to left: 'helper.js', then '../utils' which means go up one dir from 'components' to 'src', then 'components' (ignored as '../utils' changes context), then 'src', then prepends cwd)
+// console.log(path.resolve("src", "components", "./utils", "helper.js"));
+
+// console.log(path.resolve("data", "user")); // same
+// console.log(path.join(__dirname, "data", "user")); // same
+
+// console.log(
+//   path.relative("/home/user/project/src", "/home/user/project/dist/build.js")
+// );
+// Output: ../dist/build.js
--- process01/app.js
+++ process01/app.js
@@ -1,6 +1,6 @@
 console.log(process.platform);
 console.log(process.cwd());
-console.log(process.env.USER);
-console.log(process.env.HOME);
-console.log(process.env.AAA);
-console.log(process.env.BBB);
+// console.log(process.env.USER);
+// console.log(process.env.HOME);
+// console.log(process.env.AAA);
+// console.log(process.env.BBB);
