45fa874daf11939f275181758569d8e0bb6b17d3
jenwit-dev <johnny.js19@gmail.com>
2025-06-13 05:49:21 +0700

    node:event based http server

--- http01/server.js
+++ http01/server.js
@@ -2,17 +2,25 @@ const http = require("http");
 const server = http.createServer();
 server.listen(8000);
 
-const todo = "Go Home";
+const todo = ["Go Home", "Go Learn", "Go Lunch"];
+const todoHTML = todo.map((el) => `<h1>${el}</h1>`).join("");
 
 server.on("request", (req, res) => {
   console.log(req.url);
   //   console.log(req.method);
   if (req.url === "/todo") {
-    res.statusCode = 222;
-    //   res.setHeader("Content-Type", "text/plain");
-    res.setHeader("Content-Type", "text/html");
+    // res.statusCode = 222;
+    // res.setHeader("Content-Type", "text/plain");
+    // res.setHeader("Content-Type", "text/html");
+    // res.setHeader("Content-Type", "application/json");
+    // res.setHeader("We-Are", "the champion yeah");
+    res.writeHead(404, {
+      // "Content-Type": "application/json",
+      "Content-Type": "text/html",
+      "We-Are": "the World",
+    });
     res.write("<h1>Hello</h1>");
-    res.write(`<p>${todo}</p>`);
+    res.write(todoHTML);
     res.end();
   } else {
     res.statusCode = 404;
--- hw1/app.js
+++ hw1/app.js
@@ -8,10 +8,10 @@ const summary = path.resolve("resource", "summary.json");
 // console.log(summary);
 
 const getFile1 = (file) => {
-  fs.readFile(file, "utf8")
+  fs.readFile(file, "utf8") // no return on this line
     .then((result) => {
       // console.log(result);
-      return result;
+      return result; // return on this line instead
     })
     .then(console.log);
 };
@@ -24,30 +24,60 @@ const getFile3 = (file) => fs.readFile(file, "utf8");
 // getFile2(products).then((data) => console.log(data));
 // getFile3(products).then((data) => console.log(data));
 
+// getFile3(products)
+//   .then((result) => {
+//     const product = JSON.parse(result);
+//     let countClothes = 0;
+//     let countShoes = 0;
+//     let countOthers = 0;
+//     let countCamilo = 0;
+//     let countElectronics = 0;
+//     product.forEach((item) => {
+//       if (item.category.name === "Clothes") countClothes++;
+//       if (item.category.name === "Shoes") countShoes++;
+//       if (item.category.name === "Others") countOthers++;
+//       if (item.category.name === "Camilo") countCamilo++;
+//       if (item.category.name === "Electronics") countElectronics++;
+//     });
+//     const summaryObj = {
+//       all: product.length,
+//       Clothes: countClothes,
+//       Shoes: countShoes,
+//       Others: countOthers,
+//       Camilo: countCamilo,
+//       Electronics: countElectronics,
+//     };
+//     return JSON.stringify(summaryObj);
+//   })
+//   .then((summaryStr) => fs.writeFile("summary.json", summaryStr))
+//   .then(() => console.log("success"));
+
+// getFile3(products).then((result) => {
+//   const product = JSON.parse(result);
+//   const summaryArr = product.reduce(
+//     (a, c) => {
+//       if (c.category.name === "Clothes") a.Clothes += 1;
+//       a.all += 1;
+//       return a;
+//     },
+//     { all: 0, Clothes: 0 }
+//   );
+//   console.log(summaryArr);
+// });
+
 getFile3(products)
   .then((result) => {
     const product = JSON.parse(result);
-    let countClothes = 0;
-    let countShoes = 0;
-    let countOthers = 0;
-    let countCamilo = 0;
-    let countElectronics = 0;
-    product.forEach((item) => {
-      if (item.category.name === "Clothes") countClothes++;
-      if (item.category.name === "Shoes") countShoes++;
-      if (item.category.name === "Others") countOthers++;
-      if (item.category.name === "Camilo") countCamilo++;
-      if (item.category.name === "Electronics") countElectronics++;
-    });
-    const summaryObj = {
-      all: product.length,
-      Clothes: countClothes,
-      Shoes: countShoes,
-      Others: countOthers,
-      Camilo: countCamilo,
-      Electronics: countElectronics,
-    };
-    return JSON.stringify(summaryObj);
+    const summaryArr = product.reduce(
+      (a, c) => {
+        if (!a[c.category.name]) a[c.category.name] = 0;
+        a[c.category.name] += 1;
+        a.all += 1;
+        return a;
+      },
+      { all: 0 }
+    );
+    return fs.writeFile(summary, JSON.stringify(summaryArr, null, 1));
   })
-  .then((summaryStr) => fs.writeFile("summary.json", summaryStr))
-  .then(() => console.log("success"));
+  .then(() => getFile3(summary))
+  .then(console.log);
